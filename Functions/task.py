6fbf136d431d6643a0a80d8a1ef7213e78b815d9
SMHashimi <mubaris.hashimi2018@gmail.com>
2024-09-28 03:03:30 -0400

    Adding Functions to the Practice

---
+++ Functions/task.py
@@ -0,0 +1,52 @@
+# def my_function():
+#     print("Hello")
+#     print("bye")
+# my_function()
+
+#Hurdle 1, 2, and 3
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     if wall_in_front():
+#         turn_left()
+#         move()
+#         turn_right()
+#         move()
+#         turn_right()
+#         move()
+#         turn_left()
+#https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Hurdle%203&url=worlds%2Ftutorial_en%2Fhurdle3.json
+
+
+#Hurdle 4
+#https://drive.google.com/file/d/1TmOvdJIi8oIJAI4yUPx6GXCejN3nA6wB/view?usp=drive_link
+
+# def turn_right():
+#     turn_left()
+#     turn_left()
+#     turn_left()
+# while not at_goal():
+#     if front_is_clear():
+#         move()
+#     if right_is_clear():
+#         turn_right()
+#     if wall_in_front():
+#         turn_left()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
