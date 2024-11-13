b89a963e4bca67c689659ebf1f43f5201f999a08
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-13 09:34:41 -0500

    RelativeAbsolutePathFile

--- RelativeAbsoluteFilePath/RelativeFilePath.py
+++ RelativeAbsoluteFilePath/RelativeFilePath.py
@@ -1,4 +1,5 @@
-with open("my_file.txt") as my_notebook:
-    contents = my_notebook.read()
-    print(contents)
-#THE FILE MUST EXIST IN THE WORKING DIRECTORY
+with open("./myfile/my_file.txt") as my_notebook:
+     contents = my_notebook.read()
+     print(contents)
+
+# THE FILE MUST EXIST IN THE WORKING DIRECTORY
