8b8d7ac9e2308b465c0f733b96782e167e69ad03
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-12 22:25:05 -0500

    Relative&AbsoluteFilePath

---
+++ RelativeAbsoluteFilePath/AbsoluteFilePath.py
@@ -0,0 +1,5 @@
+
+with open("C:/Users/S Mubaris Hashimi/Desktop/new_file.txt") as file:
+    contents = file.read()
+    print(contents)
+#THE FILE IS NOT REQUIRED TO BE IN THE WORKING DIRECTORY
---
+++ RelativeAbsoluteFilePath/RelativeFilePath.py
@@ -0,0 +1,4 @@
+with open("my_file.txt") as my_notebook:
+    contents = my_notebook.read()
+    print(contents)
+#THE FILE MUST EXIST IN THE WORKING DIRECTORY
