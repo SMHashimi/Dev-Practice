aba6fd70f24058019f26dc351493e7d1450b82a8
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-21 07:01:17 -0500

    tkinterIntro

--- Tkinter/tkinter_01_IntroANDButton.py
+++ Tkinter/tkinter_01_IntroANDButton.py
@@ -3,13 +3,16 @@ from tkinter import *
 intro = Tk()
 intro.title("The Introduction App")
 intro.minsize(width = 800, height = 800)
+intro.config(padx = 40, pady = 40)
 #text in the interface
 my_app = Label(text = "Hey, I am Sayed!\nWelcome to the first app I created in Tkinter library of Python",
                font = ("Arial", 16, "bold", "italic", "underline"))
-my_app.pack(pady = (40, 10))
+
+# my_app.pack(pady = (40, 10))
 my_app["text"] = "Hey, I am Mubaris"
 my_app.config(text = "Hey, I am Mubaris.")
-
+# my_app.pack(pady = (40, 10))
+my_app.grid(column = 0, row = 0)
 
 def button_clicked():
     print("I got clicked!")
@@ -17,19 +20,14 @@ def button_clicked():
     my_app.config(text = new_text)
 
 button = Button(text = "Sign in Here", command = button_clicked)
-button.pack(pady = 10)
+button.grid(column = 1, row = 1)
+
 user_input = Entry(width = 35)
-user_input.pack()
+user_input.grid(column = 3, row = 2)
+# user_input.pack()
+#You cannot use the grid and pack in the same file.
 
-
-
-
-
-
-
-
+new_button = Button(text = "New button")
+new_button.grid(column = 2, row = 0)
 
 intro.mainloop()
-
-
-
