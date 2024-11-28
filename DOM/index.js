1bcffadf19a63fb42a7a0f2e74904320adae0805
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2024-11-28 09:17:12 -0500

    JavaScript

---
+++ DOM/index.js
@@ -0,0 +1,6 @@
+document.querySelector("h1").innerHTML = "Good Bye";
+document.querySelector(".list").innerHTML = "You";
+document.getElementsByTagName("li");
+document.getElementsByClassName("list")[0].style.color = "green";
+document.getElementById("he").style.color = "red";
+document.querySelector("h1").style.fontSize = "5rem";
