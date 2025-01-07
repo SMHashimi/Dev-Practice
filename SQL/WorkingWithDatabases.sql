607858337f52f3512380fd0a33627f1d166b0456
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2025-01-06 22:15:09 -0500

    SQL

--- SQL/WorkingWithDatabases.sql
+++ SQL/WorkingWithDatabases.sql
@@ -348,4 +348,24 @@ SELECT
     RANK() OVER(ORDER BY salary DESC) AS overall_rank,
     DENSE_RANK() OVER(ORDER BY salary DESC) AS overall_dense_rank
     FROM employees ORDER BY department;
+-- CREATING TRIGGERS
+CREATE DATABASE trigger_demo;
+USE trigger_demo;
+SELECT DATABASE();
+CREATE TABLE users(
+    username VARCHAR(100),
+    age INT);
+DELIMITER $$
+CREATE TRIGGER must_be_adult
+     BEFORE INSERT ON users FOR EACH ROW
+     BEGIN
+          IF NEW.age < 18
+          THEN
+              SIGNAL SQLSTATE '45000'
+                    SET MESSAGE_TEXT = 'Must be an adult!';
+          END IF;
+     END;
+$$
+DELIMITER ;    
+
 
