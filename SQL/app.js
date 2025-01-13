30768b94044e6a5f6991c8e7b2c7a19afffbd104
SayedMubarisHashimi <mubaris.hasimi2018@gmail.com>
2025-01-12 21:11:58 -0500

    SQL

---
+++ SQL/app.js
@@ -0,0 +1,9 @@
+// In for loop, there are three parts: initialization, condition, and increment
+for(var i = 0; i < 500; i++) {
+    console.log("THIS IS SOME CODE");
+}
+for(var i = 0; i < 500; i++) {
+    // INSERT A NEW USER INTO THE DATABASE (MySQL) 
+}
+// packages are installed through npm (NODE PACKAGE MANAGER)
+var {faker} = require("@faker-js/faker")
